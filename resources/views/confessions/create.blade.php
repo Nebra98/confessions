512ff3d02181ef4a5efdaf3f2852a9d379e3e508
Nebra98 <bulicbrane561@gmail.com>
2023-03-13 12:15:18 +0100

    Error handling, keep old data in the form if validation didn't pass

--- resources/views/confessions/create.blade.php
+++ resources/views/confessions/create.blade.php
@@ -8,11 +8,18 @@
                     @csrf
                     <div class="mb-3">
                         <label for="user_name" class="form-label">Korisničko ime</label>
-                        <input type="text" class="form-control" name="user_name" placeholder="Anonymous" value="Anonymous">
+                        <input type="text" class="form-control" name="user_name" placeholder="Anonymous" value="{{ old('user_name') ?? 'Anonymous' }}" required>
+                        @error('user_name')
+                        <p class="text-danger text-xs mt-1">{{ $message }}</p>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="confession" class="form-label">Ostavi ispovijed ovdje</label>
-                        <textarea class="form-control" name="confession" rows="3"></textarea>
+                        <textarea class="form-control" name="confession" rows="8" required>{{ old('confession') }}</textarea>
+
+                        @error('confession')
+                        <p class="text-danger text-xs mt-1">{{ $message }}</p>
+                        @enderror
                     </div>
                     <button type="submit" class="btn btn-secondary">Submit</button>
                 </form>
