f133050d210a8c645d4d874a6d94fd722797a868
Nebra98 <bulicbrane561@gmail.com>
2023-03-13 10:44:53 +0100

    Added ConfessionController

---
+++ app/Http/Controllers/ConfessionController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Confession;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ConfessionController extends Controller
+{
+
+}
