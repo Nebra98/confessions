54be4590175685582c89ea66659bf352c45f9098
Nebra98 <bulicbrane561@gmail.com>
2023-03-19 16:36:19 +0100

    Comments table (added fields)

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'parent_comment_id', 'user_id', 'confession_id', 'comment', 'comment_sender_name'
+    ];
+
 }
--- database/migrations/2023_03_14_223948_create_comments_table.php
+++ database/migrations/2023_03_14_223948_create_comments_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('parent_comment_id')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('confession_id')->nullable();
+            $table->text('comment', 1000);
+            $table->text('comment_sender_name');
             $table->timestamps();
+            $table->foreign('parent_comment_id')->references('id')->on('comments')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('confession_id')->references('id')->on('confessions')->onDelete('cascade');
         });
     }
 
