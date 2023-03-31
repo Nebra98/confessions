0b3c70fa6d0631bdcb2bf92dc39dfd2035b16234
Nebra98 <bulicbrane561@gmail.com>
2023-03-31 02:33:26 +0200

    Added character counting for textarea

--- resources/views/confessions/create.blade.php
+++ resources/views/confessions/create.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
 @section('content')
     <div class="container">
@@ -14,12 +15,15 @@
                         @enderror
                     </div>
                     <div class="mb-3">
-                        <label for="confession" class="form-label">Ostavi ispovijed ovdje</label>
-                        <textarea class="form-control" name="confession" rows="8" required>{{ old('confession') }}</textarea>
+                        <div class="form-group">
+                            <label for="confession" class="form-label">Ostavi ispovijed ovdje</label>
+                            <textarea class="form-control count" name="confession" id="confession" rows="8" maxlength="1000" required>{{ old('confession') }}</textarea>
+                            <small class="form-text text-muted">0 characters remaining</small>
 
-                        @error('confession')
-                        <p class="text-danger text-xs mt-1">{{ $message }}</p>
-                        @enderror
+                            @error('confession')
+                            <p class="text-danger text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
                     </div>
                     <button type="submit" class="btn btn-secondary">Submit</button>
                 </form>
@@ -27,3 +31,32 @@
         </div>
     </div>
 @endsection
+
+<script>
+
+    function countCharacters() {
+        var max = $(this).attr("maxlength");
+        var length = $(this).val().length;
+        var counter = max - length;
+        var helper = $(this).next(".form-text");
+        // Switch to the singular if there's exactly 1 character remaining
+        if (counter !== 1) {
+            helper.text(counter + " characters remaining");
+        } else {
+            helper.text(counter + " character remaining");
+        }
+        // Make it red if there are 0 characters remaining
+        if (counter === 0) {
+            helper.removeClass("text-muted");
+            helper.addClass("text-danger");
+        } else {
+            helper.removeClass("text-danger");
+            helper.addClass("text-muted");
+        }
+    }
+
+    $(document).ready(function () {
+        $(".count").each(countCharacters);
+        $(".count").keyup(countCharacters);
+    });
+</script>
